9f43141be4517e2cde3db1fa0d9101672fe4c205
Level237 <Bramslevel129@gmail.com>
2023-02-22 19:52:34 +0100

    logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -916,7 +916,15 @@
 
                         <div class="dropdown-divider"></div>
 
-                        <a class="dropdown-item" href="#">Sign out</a>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
                       </div>
                     </div>
                     <!-- End Account -->
